31c0ced66b78aa1d0337fb60e8587b5c387971f2
iqbalsafeip <isaacfrost@gmail.com>
2025-10-30 16:39:48 +0700

    a

---
+++ src/lib/components/layout/navbar/navbar-menu.ts
@@ -0,0 +1,22 @@
+export const navbarMenu = [
+	{
+		title: 'home',
+		href: '/',
+		key: 'h'
+	},
+	{
+		title: 'abouts',
+		href: '/abouts',
+		key: 'a'
+	},
+	{
+		title: 'guest-book',
+		href: '/guest-book',
+		key: 'g'
+	},
+	{
+		title: 'articles',
+		href: '/articles',
+		key: 'r'
+	}
+] as const;
